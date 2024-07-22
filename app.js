58e03f9388bd61b7fa6674021c8844170e631dca
Bhadra-design <gbhadra919@gmail.com>
2024-07-22 20:08:51 +0530

    implementing vuejs using cdn and prinitng dynamic data using the data method

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data () {
+    return {
+      title: "Red Bull Racing",
+      driver1: "Max Verstappen",
+      driver2: "Sergio Perez",
+    }
+  }
+});
+
+app.mount("#app");
