44dfac90d1bc1e6b9fef5b6b7c62c0964889fb95
Bhadra-design <gbhadra919@gmail.com>
2024-07-23 19:27:54 +0530

    dynamic classes

--- app.js
+++ app.js
@@ -4,9 +4,30 @@ const app = Vue.createApp({
       url: "https://www.youtube.com",
       showDets: true,
       drivers: [
-        {image: 'assets/red bull.avif', team: "Red Bull", driver1: "Max Verstappen", driver2: "Sergio Perez", points: 69},
-        {image: 'assets/ferrari.avif', team: "Ferrari", driver1: "Charles Leclerc", driver2: "Carlos Sainz", points: 68},
-        {image: 'assets/mercedes.avif', team: "Mercedes", driver1: "Lewis Hamilton", driver2: "George Russell", points: 45},
+        {
+          image: "assets/red bull.avif",
+          team: "Red Bull",
+          driver1: "Max Verstappen",
+          driver2: "Sergio Perez",
+          points: 69,
+          isFav: true
+        },
+        {
+          image: "assets/ferrari.avif",
+          team: "Ferrari",
+          driver1: "Charles Leclerc",
+          driver2: "Carlos Sainz",
+          points: 68,
+          isFav: true
+        },
+        {
+          image: "assets/mercedes.avif",
+          team: "Mercedes",
+          driver1: "Lewis Hamilton",
+          driver2: "George Russell",
+          points: 45,
+          isFav: false
+        },
       ],
       x: 0,
       y: 0,
@@ -28,7 +49,7 @@ const app = Vue.createApp({
     handleMouse(e) {
       this.x = e.offsetX;
       this.y = e.offsetY;
-    }
+    },
   },
 });
 
