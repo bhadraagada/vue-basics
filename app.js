9ffd40d9e781f707ea9fd6c9418a1dce0cad5550
Bhadra-design <gbhadra919@gmail.com>
2024-07-23 18:59:59 +0530

    learned some basic mouse events

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const app = Vue.createApp({
-  data () {
+  data() {
     return {
       showDets: true,
       title: "Red Bull Racing",
       driver1: "Max Verstappen",
       driver2: "Sergio Perez",
-      points: 60
-    }
+      points: 60,
+      x: 0,
+      y: 0,
+    };
   },
   methods: {
     changeDriver(driver2) {
@@ -15,8 +17,17 @@ const app = Vue.createApp({
     },
     toggleShow() {
       this.showDets = !this.showDets;
+    },
+    // mouse events
+    handleEvent(e, i) {
+      console.log(e, e.type);
+      if (i) console.log(i);
+    },
+    handleMouse(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     }
-  }
+  },
 });
 
 app.mount("#app");
