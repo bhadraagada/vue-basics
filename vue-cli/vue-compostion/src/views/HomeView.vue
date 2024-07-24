7db32ccf0ae734e5a6d3f789216a3b9eff1ecfa4
Bhadra-design <gbhadra919@gmail.com>
2024-07-24 21:02:15 +0530

    compostion api intro

---
+++ vue-cli/vue-compostion/src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    Home
+    <p>name: {{ name }}</p>
+    <p ref="p">age: {{ age }}</p>
+    <button @click="handleClick">Click me </button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: "Home",
+  setup() {
+    console.log(this);
+
+    const p = ref("hello");
+
+    let name = "bhadra";
+    let age = 19;
+
+    const handleClick = () => {
+      console.log(p.value);
+      p.value.classList.add("active");
+      p.value.textContent = "hello world";
+    };
+
+    return { name, age, handleClick, p };
+  },
+};
+</script>
